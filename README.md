09ef7f5b6050dcbd9514a9a6257418e05abd42b2
Shalin Shah <44682269+shalin-s@users.noreply.github.com>
2022-03-17 07:58:39 -0400

    Updates 2022-03-17
    -Restructured all of the code to be sensibly-organized and modular, and to store/persist intermediate
    results.
    -Added linear regression model.
    -Other minor code improvements.
    -Updated readme.
    -Added .gitignore.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 10-707 Final Project: Traffic Forecasting
+
+common.py contains constants (dataset file paths and such) to be used throughout this project.
+
+util.py contains utility functions/classes to be used throughout this project.
+
+preprocessing.ipynb loads dataset and performs data preprocessing.
+
+model_X.ipynb for any X (e.g. Feedforward, LinearRegression) trains a model and generates predictions, then saves/serializes them.
+
+evaluate.ipynb loads dataset, loads model, and evaluates model (specified by model_name within) on dataset.
---
+++ common.py
@@ -0,0 +1,24 @@
+import os
+
+# CONSTANTS (modify as needed)
+
+# DATA_DIRECTORY = "/Users/varunursekar/Projects/advDeepLearning/final_project/data"
+DATA_DIRECTORY = r"C:\Users\shali\Documents\CMU_10-707_Project\data"
+DATASET_NAME = "METR-LA"
+OUTPUT_DIRECTORY = DATA_DIRECTORY
+PLOTS_DIRECTORY = OUTPUT_DIRECTORY
+
+PREPROCESSED_DATASET_FILENAME = DATASET_NAME + "_preprocessed.npz"
+PREPROCESSING_SCALER_FILENAME = DATASET_NAME + "_scaler.pk"
+
+DATASET_DIRECTORY = os.path.join(DATA_DIRECTORY, DATASET_NAME)
+PREPROCESSED_DATASET_FILEPATH = os.path.join(OUTPUT_DIRECTORY, PREPROCESSED_DATASET_FILENAME)
+PREPROCESSING_SCALER_FILEPATH = os.path.join(OUTPUT_DIRECTORY, PREPROCESSING_SCALER_FILENAME)
+
+def model_name_to_model_filepath(model_name):
+    filename = "_".join([DATASET_NAME, model_name, "model"]) + ".h5"
+    return os.path.join(OUTPUT_DIRECTORY, filename)
+
+def model_name_to_run_info_filepath(model_name):
+    filename = "_".join([DATASET_NAME, model_name, "run_info"]) + ".pk"
+    return os.path.join(OUTPUT_DIRECTORY, filename)
