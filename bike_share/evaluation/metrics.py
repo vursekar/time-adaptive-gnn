35f23fef428a940b08ad866bef128926db48df37
Varun Ursekar <varunursekar@Varuns-MacBook-Pro.local>
2022-04-24 09:27:24 -0400

    fixed metrics and plots

--- bike_share/evaluation/metrics.py
+++ bike_share/evaluation/metrics.py
@@ -1,12 +1,15 @@
 import numpy as np
 
-def mae_losses(preds,labels):
-    return np.mean(np.abs(preds-labels),axis=(0,2))[:,0]
+def mae_losses(preds,labels,axes=(0,1)):
+    return np.mean(np.abs(preds-labels),axis=axes)
 
-def mape_losses(preds,labels):
+def mape_losses(preds,labels,axes=(0,1)):
     return np.mean(np.abs(np.divide(preds-labels,labels,
                                     out=np.zeros_like(preds), 
-                                    where=labels!=0)),axis=(0,2))[:,0]
+                                    where=labels!=0)),axis=axes)
 
-def rmse_losses(preds,labels):
-    return np.mean(np.sqrt(np.mean(np.square(preds-labels),axis=2)),axis=0)[:,0]
+def mse_losses(preds,labels,axes=(0,1)):
+    return np.mean(np.square(preds-labels),axis=axes)
+
+def smape_losses(preds,labels,axes=(0,1)):
+    return np.mean(np.abs(preds-labels)/(np.abs(preds) + np.abs(labels)),axis=axes)
